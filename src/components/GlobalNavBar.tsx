75b5c45f21557316574116438e7c4c01e503812e
Malcolm <hauser.malcolm@gmail.com>
2024-11-10 16:31:12 +1000

    make the search button search

--- src/components/GlobalNavBar.tsx
+++ src/components/GlobalNavBar.tsx
@@ -20,8 +20,6 @@ function GlobalNavBar() {
 	const [query, setQuery] = useState("");
 	const [shouldShowResults, setShouldShowResults] = useState(true);
 	const [results, setResults] = useState<SearchResult[]>([]);
-
-	// Check if the page is /search
 	const location = useLocation();
 	const isSearchPage = location.pathname === "/search";
 
@@ -59,7 +57,10 @@ function GlobalNavBar() {
 		}, 1000);
 	}
 
-	function handleKeyDown(e: any) {
+	function handleKeyDown(e: any, override = false) {
+		if (override) {
+			window.location.href = `/search?query=${query}`;
+		}
 		if (e.key == "Enter") {
 			window.location.href = `/search?query=${query}`;
 		}
@@ -76,7 +77,7 @@ function GlobalNavBar() {
 				{!isSearchPage && (
 					<li className="navLink" style={{ position: "fixed", right: "10px" }}>
 						<div className="searchBar" style={{ marginInline: "auto" }}>
-							<svg height="24" viewBox="0 0 64 64" width="24" xmlns="http://www.w3.org/2000/svg">
+							<svg height="24" viewBox="0 0 64 64" width="24" xmlns="http://www.w3.org/2000/svg" onClick={(e) => handleKeyDown(e, true)}>
 								<path d="m26.72 50.414c5.205 0 10.005-1.683 13.932-4.488l14.773 14.773c.686.686 1.59 1.028 2.556 1.028 2.026 0 3.46-1.558 3.46-3.553 0-.935-.312-1.807-.998-2.493l-14.68-14.71c3.086-4.052 4.925-9.07 4.925-14.524 0-13.184-10.784-23.968-23.967-23.968-13.153 0-23.968 10.753-23.968 23.968 0 13.183 10.784 23.967 23.968 23.967zm0-5.174c-10.285 0-18.793-8.508-18.793-18.793 0-10.286 8.508-18.794 18.794-18.794 10.285 0 18.793 8.508 18.793 18.794 0 10.285-8.508 18.793-18.793 18.793z" />
 							</svg>
 							<input type="text" value={query} onChange={(e) => handleSearchChange(e)} onClick={() => handleSearchClick()} onBlur={() => handleOnBlur()} onKeyDown={(e) => handleKeyDown(e)} placeholder="Search" />
