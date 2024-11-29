9d37664a67798bdda06016ffc579b6dcb680f5d7
Malcolm <hauser.malcolm@gmail.com>
2024-11-29 18:52:59 +1000

    If a user is logged out and clicks add to watch list send them to the login page

--- src/utils/watchlist.ts
+++ src/utils/watchlist.ts
@@ -1,17 +1,28 @@
 import { postJSONData, deleteJSONData } from "./api";
 
-function addToWatchlist(name: string, urlName: string, thumbnailURL: string, setIsInWatchlist: any) {
+async function addToWatchlist(name: string, urlName: string, thumbnailURL: string, setIsInWatchlist: any) {
 	const username = localStorage.getItem("username") || "";
 	const id = localStorage.getItem("id") || "";
+	const data = await postJSONData(`https://api.hexagon.kiwi-micro.com:8072/addToWatchlist`, { name, urlName, thumbnailURL, username, id });
+
+	if ((data.error = "Invalid credentials")) {
+		window.location.href = "/login";
+		return;
+	}
 	setIsInWatchlist(true);
-	postJSONData(`https://api.hexagon.kiwi-micro.com:8072/addToWatchlist`, { name, urlName, thumbnailURL, username, id });
 }
 
-function removeFromWatchlist(urlName: string, setIsInWatchlist: any) {
+async function removeFromWatchlist(urlName: string, setIsInWatchlist: any) {
 	const username = localStorage.getItem("username") || "";
 	const id = localStorage.getItem("id") || "";
+	const data = await deleteJSONData(`https://api.hexagon.kiwi-micro.com:8072/removeFromWatchlist`, { urlName, username, id });
+
+	if ((data.error = "Invalid credentials")) {
+		console.log("o");
+		window.location.href = "/login";
+		return;
+	}
 	setIsInWatchlist(false);
-	deleteJSONData(`https://api.hexagon.kiwi-micro.com:8072/removeFromWatchlist`, { urlName, username, id });
 }
 
 export { addToWatchlist, removeFromWatchlist };
