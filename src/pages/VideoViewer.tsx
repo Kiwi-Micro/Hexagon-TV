182ab7b28deb744324edcf2bc64557fc2ea371af
Malcolm <hauser.malcolm@gmail.com>
2025-01-17 15:39:06 +1000

    its basic

--- src/pages/VideoViewer.tsx
+++ src/pages/VideoViewer.tsx
@@ -1,5 +1,6 @@
-import backArrow from "../assets/img/backArrow.png";
-import { useState, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import "../assets/video.css";
+import backButton from "../assets/img/backArrow.png";
 
 interface ProductProps {
 	name: string;
@@ -7,75 +8,176 @@ interface ProductProps {
 	previousPage: string;
 }
 
-function VideoViewer({
-	name,
-	videoURL,
-	previousPage,
-}: ProductProps) {
+function VideoViewer({ name, videoURL, previousPage }: ProductProps) {
+	document.title = "Hexagon TV | Watching " + name;
+	const videoRef = useRef<HTMLVideoElement | null>(null);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [currentTime, setCurrentTime] = useState(0);
+	const [duration, setDuration] = useState(0);
 	const [active, setActive] = useState(true);
-	const hideControlsDelay = 2000;
+
+	function formatTime(seconds: number): string {
+		const hours = Math.floor(seconds / 3600);
+		const minutes = Math.floor((seconds % 3600) / 60);
+		const remainingSeconds = Math.floor(seconds % 60);
+
+		if (hours > 0) {
+			return `${hours}:${minutes
+				.toString()
+				.padStart(2, "0")}:${remainingSeconds
+				.toString()
+				.padStart(2, "0")}`;
+		} else {
+			return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+		}
+	}
+
+	const hideControlsDelay = 3000;
 	let hideControlsTimeout: any;
+
 	function hideControls() {
 		setActive(false);
 	}
+
 	function handleMouseMove() {
 		setActive(true);
 		clearTimeout(hideControlsTimeout);
-		hideControlsTimeout = setTimeout(
-			hideControls,
-			hideControlsDelay,
-		);
+		hideControlsTimeout = setTimeout(hideControls, hideControlsDelay);
 	}
+
+	function playPauseVideo() {
+		if (!videoRef.current) return;
+		if (isPlaying) {
+			videoRef.current.pause();
+		} else {
+			videoRef.current.play();
+		}
+		setIsPlaying(!isPlaying);
+	}
+
+	const handleKeyDown = (e: KeyboardEvent) => {
+		if (!videoRef.current) return;
+		if (e.key === "ArrowLeft") {
+			videoRef.current.currentTime -= 5;
+			handleMouseMove();
+		} else if (e.key === "ArrowRight") {
+			videoRef.current.currentTime += 5;
+			handleMouseMove();
+		} else if (e.key === " ") {
+			playPauseVideo();
+		}
+	};
+
 	useEffect(() => {
-		hideControlsTimeout = setTimeout(
-			hideControls,
-			hideControlsDelay,
-		);
-		document.addEventListener(
-			"mousemove",
-			handleMouseMove,
-		);
+		window.addEventListener("keydown", handleKeyDown);
+
+		hideControlsTimeout = setTimeout(hideControls, hideControlsDelay);
+		document.addEventListener("mousemove", handleMouseMove);
+
 		return () => {
 			clearTimeout(hideControlsTimeout);
-			document.removeEventListener(
-				"mousemove",
-				handleMouseMove,
-			);
+			document.removeEventListener("mousemove", handleMouseMove);
 			clearTimeout(hideControlsTimeout);
+
+			window.removeEventListener("keydown", handleKeyDown);
 		};
 	}, []);
-	document.title =
-		"Hexagon TV | Watching " + name;
+
+	function handleTimeUpdate() {
+		if (!videoRef.current) return;
+		setCurrentTime(videoRef.current.currentTime);
+	}
+
+	function handleLoadedMetadata() {
+		if (!videoRef.current) return;
+		setDuration(videoRef.current.duration);
+	}
+
+	function handleSeek(
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+	) {
+		if (!videoRef.current) return;
+		const rect = event.currentTarget.getBoundingClientRect();
+		const seekTime =
+			((event.clientX - rect.left) / rect.width) *
+			videoRef.current.duration;
+		videoRef.current.currentTime = seekTime;
+		setCurrentTime(seekTime);
+	}
+
 	return (
-		<div className="main">
-			<div className="videoPage">
-				<p
-					onClick={() => {
-						window.location.href = previousPage;
-					}}
-					className="videoPageHeaderNav"
-					style={
-						active
-							? { display: "flex" }
-							: { display: "none" }
-					}
-				>
-					<img
-						src={backArrow}
-						draggable="false"
-						height={32}
-						alt="back arrow"
-					/>
-					<p style={{ marginTop: "5px" }}>
-						{name}
-					</p>
-				</p>
-				<video
-					src={videoURL}
-					controls={active}
-					className="video"
-					id="myVideo"
+		<div className="videoPage main">
+			<div
+				className="videoPageBackButtonDiv"
+				style={{ display: active ? "flex" : "none" }}
+				onClick={() => (window.location.href = previousPage)}
+			>
+				<img
+					src={backButton}
+					className="videoPageBackButton"
+					alt="back button"
+					draggable="false"
 				/>
+				<h3>Back</h3>
+			</div>
+			<video
+				src={videoURL}
+				className="videoPageVideo"
+				ref={videoRef}
+				onClick={playPauseVideo}
+				onEnded={playPauseVideo}
+				onTimeUpdate={handleTimeUpdate}
+				onLoadedMetadata={handleLoadedMetadata}
+			/>
+			<div
+				className="videoPageControls"
+				style={{ display: active ? "flex" : "none" }}
+			>
+				<svg
+					viewBox="0 0 100 100"
+					width="32"
+					height="32"
+					style={{
+						color: "#ffffff",
+					}}
+					xmlns="http://www.w3.org/2000/svg"
+					onClick={playPauseVideo}
+				>
+					{isPlaying ? (
+						<>
+							<rect
+								x="30"
+								y="20"
+								width="15"
+								height="60"
+								fill="currentColor"
+							/>
+							<rect
+								x="55"
+								y="20"
+								width="15"
+								height="60"
+								fill="currentColor"
+							/>
+						</>
+					) : (
+						<polygon points="30,20 80,50 30,80" fill="currentColor" />
+					)}
+				</svg>
+				<div
+					className="playbackBarContainer"
+					onClick={handleSeek}
+					role="button"
+					aria-label="Seek through video"
+				>
+					<div
+						className="playbackBar"
+						style={{
+							width: `${(currentTime / duration) * 100 || 0}%`,
+						}}
+					/>
+				</div>
+				<p>{formatTime(currentTime)}</p>
 			</div>
 		</div>
 	);
