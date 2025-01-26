c3bf5202ae1c6a03bb434d65bc34ab41d8d49951
Malcolm <hauser.malcolm@gmail.com>
2025-01-26 17:52:15 +1000

    clean up

--- src/utils/renders.tsx
+++ src/utils/renders.tsx
@@ -12,7 +12,6 @@ function renderVideoCard(db: any) {
 			return (
 				<VideoCard
 					key={video.urlName}
-					id={video.id}
 					name={video.name}
 					videoLink={`${video.urlName}.html`}
 					thumbnailURL={video.thumbnailURL}
